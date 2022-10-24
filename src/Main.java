48a55f6b4bd92151d0b04961d4ec6aaddecbf1c5
blazej <wituckib@gmail.com>
2022-10-24 21:04:53 +0200

    konkaatenacja czyali dodawanie, 2 sanry (2x wpisz warość) i wynik alles

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
 
+        System.out.println("Wpisz swoje imię");
+        String name=sc.nextLine();
+        int year=2023;
+        int age=28;
+        System.out.println("Hello "+name+" mam lat "+age);
+        System.out.println("Hello "+name+" in "+year);
         System.out.println("Podaj wartość nr 1");
         int number1 = sc.nextInt();
         System.out.println("Podaj wartość nr 2");
